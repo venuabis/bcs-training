dd737ebcbfafb868a07bcbb58f6dd0b905ce3560
venabis <abishke0310@gmail.com>
2025-06-27 12:27:39 +0530

    day8

--- day8/08_day.js
+++ day8/08_day.js
@@ -5,4 +5,33 @@ function sayHello()
         console.log("hello!");
     }
 }
-console.log(sayHello());
+     //sayHello()();
+    // function can be treated as value
+    // function  -> value
+    // function  ==> first class ciizen
+    // 1. value can be passed as arg
+    // 2. value can be returned
+    // 3. value can be assigned
+
+   // const sayHello1 = () => () => console.log("Hello");
+//------------------------------------------------
+//      1. f#
+//      2. scala
+//      3. lisp
+//      4. small
+//      5. haskell   
+
+//--------------------------------------------------
+
+// functional programing style. (paradigm)
+     // 1 .currying
+     // 2 .partial application.
+     // 3. point free style.
+
+
+
+
+    const add = (x) => (y) => x + y;
+    
+    console.log(add(5)(9));
+    
